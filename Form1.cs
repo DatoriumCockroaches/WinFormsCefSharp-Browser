8e9eaf68710bb1c85c9954d5afa5952a7edf42fe
Egons Utinans <uegons@gmail.com>
2023-03-09 17:34:59 +0200

    nearly final changes

--- Form1.cs
+++ Form1.cs
@@ -404,52 +404,6 @@ namespace ChromiumBrowser
             }
         }
 
-        private void MainSearchBarKeyDown(object sender, KeyEventArgs e)
-        {
-            if (e.KeyCode == Keys.Enter)
-            {
-                var tabPage = BrowserTabs.SelectedTab;
-                e.SuppressKeyPress = true;
-
-                // Get the ChromiumWebBrowser control from the tab page
-                chromiumBrowser = tabPage.Controls.OfType<ChromiumWebBrowser>().FirstOrDefault();
-
-                string Pattern = @"^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$";
-                Regex Rgx = new Regex(Pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
-
-                if (tabPage.Text == "History")
-                {
-                    chromiumBrowser = new ChromiumWebBrowser();
-                    chromiumBrowser.AddressChanged += OnBrowserAddressChanged;
-                    chromiumBrowser.FrameLoadEnd += browser_FrameLoadEnd;
-                    chromiumBrowser.Dock = DockStyle.Fill;
-
-                    while (tabPage.Controls.Count > 0)
-                    {
-                        Control control = tabPage.Controls[0];
-                        tabPage.Controls.Remove(control);
-                    }
-                    tabPage.Controls.Add(chromiumBrowser);
-                }
-                if (tabPage.Text.StartsWith("Tab"))
-                {
-                    chromiumBrowser = new ChromiumWebBrowser();
-                    chromiumBrowser.AddressChanged += OnBrowserAddressChanged;
-                    chromiumBrowser.FrameLoadEnd += browser_FrameLoadEnd;
-                    chromiumBrowser.Dock = DockStyle.Fill;
-
-                    while (tabPage.Controls.Count > 0)
-                    {
-                        Control control = tabPage.Controls[0];
-                        tabPage.Controls.Remove(control);
-                    }
-                    tabPage.Controls.Add(chromiumBrowser);
-                }
-
-                SearchAdress(chromiumBrowser, Address.Text);
-            }
-        }
-
         private void browser_FrameLoadEnd(object sender, FrameLoadEndEventArgs e)
         {
             if (e.Frame.IsMain)
@@ -571,31 +525,6 @@ namespace ChromiumBrowser
             BrowserTabs.BackColor = color;
         }
 
-    }
-
-    public static class ControlHelper
-    {
-        #region Redraw Suspend/Resume
-        [DllImport("user32.dll", EntryPoint = "SendMessageA", ExactSpelling = true, CharSet = CharSet.Ansi, SetLastError = true)]
-        private static extern int SendMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
-        private const int WM_SETREDRAW = 0xB;
-
-        public static void SuspendDrawing(this Control target)
-        {
-            SendMessage(target.Handle, WM_SETREDRAW, 0, 0);
-        }
-
-        public static void ResumeDrawing(this Control target) { ResumeDrawing(target, true); }
-        public static void ResumeDrawing(this Control target, bool redraw)
-        {
-            SendMessage(target.Handle, WM_SETREDRAW, 1, 0);
-
-            if (redraw)
-            {
-                target.Refresh();
-            }
-        }
-        #endregion
     }
 }
 
